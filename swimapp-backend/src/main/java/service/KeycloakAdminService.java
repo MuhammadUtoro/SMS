ddc647f7f168331f9d3d1ad284b93467a6448559
Dio Utoro <dioutoro@googlemail.com>
2026-07-30 09:14:38 +0200

    Set user password and assign role: PARENT

--- swimapp-backend/src/main/java/service/KeycloakAdminService.java
+++ swimapp-backend/src/main/java/service/KeycloakAdminService.java
@@ -1,6 +1,7 @@
 package service;
 
 import java.util.UUID;
+import java.util.List;
 
 import dto.parent.ParentRegistrationRequestDTO;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -8,12 +9,15 @@ import jakarta.ws.rs.core.Response;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 @ApplicationScoped
 public class KeycloakAdminService {
 
     private Keycloak keycloak;
 
+    // Keycloak Admin
     public KeycloakAdminService() {
         keycloak = KeycloakBuilder.builder()
                 .serverUrl("http://localhost:8081")
@@ -24,7 +28,8 @@ public class KeycloakAdminService {
                 .password("admin")
                 .build();
     }
-
+    
+    // Create user
     public UUID createUser(ParentRegistrationRequestDTO dto) {
         UserRepresentation user =  new UserRepresentation();
 
@@ -34,16 +39,39 @@ public class KeycloakAdminService {
         user.setLastName(dto.lastName());
         user.setEnabled(true);
 
+        // Set password
+        CredentialRepresentation credential =  new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(dto.password());
+        credential.setTemporary(false);
+
+        user.setCredentials(List.of(credential));
+
         Response response = keycloak.realm("dio-project").users().create(user);
         if (response.getStatus() != 201) {
             throw new RuntimeException("Failed to create Keycloak user!");
         }
 
+        // Fetch user ID
         String location = response.getHeaderString("Location");
         
         String id = location.substring(
                 location.lastIndexOf('/') + 1
                 );
+
+        // Add role - PARENT
+        RoleRepresentation parentRole = keycloak.realm("dio-project")
+                                                .roles()
+                                                .get("PARENT")
+                                                .toRepresentation();
+        // Assign role - PARENT
+        keycloak.realm("dio-project")
+                .users()
+                .get(id) // Id that we fetch
+                .roles()
+                .realmLevel()
+                .add(List.of(parentRole));
+
         return UUID.fromString(id);
     }
 
