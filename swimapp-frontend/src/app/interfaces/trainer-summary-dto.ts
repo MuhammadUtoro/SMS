95c737e61c3b6b6a4fc30ed5a9d1e48e3581ec01
Dio Utoro <dioutoro@googlemail.com>
2026-09-06 22:29:20 +0200

    add trainerId on the attribute

--- swimapp-frontend/src/app/interfaces/trainer-summary-dto.ts
+++ swimapp-frontend/src/app/interfaces/trainer-summary-dto.ts
@@ -1,6 +1,7 @@
 import { CourseDetailDto } from "./course-detail-dto";
 
 export interface TrainerSummaryDto {
+  trainerId: number;
   firstName: string;
   lastName: string;
   courses: CourseDetailDto[] | null;
