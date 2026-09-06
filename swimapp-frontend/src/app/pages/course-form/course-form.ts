99a7f0f55199c7e28f8b36927f83565f21d3973b
Dio Utoro <dioutoro@googlemail.com>
2026-09-06 22:30:26 +0200

    complete form and add dropdown for trainer

--- swimapp-frontend/src/app/pages/course-form/course-form.ts
+++ swimapp-frontend/src/app/pages/course-form/course-form.ts
@@ -1,12 +1,16 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatTimepickerModule } from '@angular/material/timepicker';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { CourseService } from '../../services/course/course.service';
+import { TrainerService } from '../../services/trainer/trainer.service';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { TrainerSummaryDto } from '../../interfaces/trainer-summary-dto';
 
 @Component({
   selector: 'app-course-form',
@@ -15,16 +19,29 @@ import { CourseService } from '../../services/course/course.service';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatTimepickerModule,
     MatSelectModule,
     ReactiveFormsModule
   ],
   templateUrl: './course-form.html',
   styleUrl: './course-form.css',
+  providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CourseForm {
+export class CourseForm implements OnInit{
   private courseService: CourseService = inject(CourseService);
   private router: Router = inject(Router);
+  private trainerService: TrainerService = inject(TrainerService);
+
+  trainers: TrainerSummaryDto[] = [];
+
+  ngOnInit() {
+    this.trainerService.getAllTrainers().subscribe({
+      next: (trainers) => {
+        this.trainers = trainers;
+      }
+    });
+  }
 
   days: string[] = [
     'Monday',
