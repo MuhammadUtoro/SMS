e456dad6e7140bd8d1396840d039178c9963fd24
Dio Utoro <dioutoro@googlemail.com>
2026-09-09 17:00:35 +0200

    modify course and level list to load list of courses and levels

--- swimapp-frontend/src/app/pages/course-list/course-list.ts
+++ swimapp-frontend/src/app/pages/course-list/course-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { CourseService } from '../../services/course/course.service';
+import { CourseSummaryDto } from '../../interfaces/course-summary-dto';
 
 @Component({
   selector: 'app-course-list',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {
+export class CourseList implements OnInit{
+  private courseService: CourseService = inject(CourseService);
 
+  courses = signal<CourseSummaryDto[]>([]);
+
+  ngOnInit(): void {
+      this.getCoursesList();
+  }
+
+  getCoursesList(){
+    this.courseService.getAllCourses().subscribe({
+      next: (courses) => {
+        this.courses.set(courses);
+      },
+      error: error => {
+        console.log("Failed to load courses!", error);
+      }
+    });
+  }
 }
--- swimapp-frontend/src/app/pages/level-list/level-list.ts
+++ swimapp-frontend/src/app/pages/level-list/level-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { LevelService } from '../../services/level/level.service';
+import { LevelSummaryDto } from '../../interfaces/level-summary-dto';
 
 @Component({
   selector: 'app-level-list',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './level-list.html',
   styleUrl: './level-list.css',
 })
-export class LevelList {
+export class LevelList implements OnInit{
+  private levelService: LevelService = inject(LevelService);
 
+  levels = signal<LevelSummaryDto[]>([]);
+
+  ngOnInit(): void {
+    this.getLevelsList();
+  }
+
+  getLevelsList() {
+    this.levelService.getAllLevels().subscribe({
+      next: (levels) => {
+        this.levels.set(levels);
+      },
+      error: error => {
+        console.log("Failed to load levels", error);
+      }
+    });
+  }
 }
