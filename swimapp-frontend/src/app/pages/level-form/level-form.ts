f57e29ef6f03ddd44f89586e0e4ac94dc85d750d
Dio Utoro <dioutoro@googlemail.com>
2026-09-09 12:30:17 +0200

    add create level form

--- swimapp-frontend/src/app/pages/level-form/level-form.ts
+++ swimapp-frontend/src/app/pages/level-form/level-form.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { LevelService } from '../../services/level/level.service';
+
+
+
 
 @Component({
   selector: 'app-level-form',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './level-form.html',
   styleUrl: './level-form.css',
 })
 export class LevelForm {
+  private levelService: LevelService = inject(LevelService);
+  private router: Router = inject(Router);
+  form: FormGroup = new FormGroup({
+    levelName: new FormControl(''),
+  });
 
+  createLevel() {
+    this.levelService.createLevel(this.form.value).subscribe({
+      next: (response) => {
+        console.log('Success', response);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.log('Failed to create course', error);
+      }
+    })
+  }
 }
