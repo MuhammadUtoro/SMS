ef0cf5d0dbce27679e3efed9522c797bfdec9b48
Dio Utoro <dioutoro@googlemail.com>
2026-09-09 16:08:08 +0200

    add dynamic data example on admin dashboard

--- swimapp-frontend/src/app/components/dashboard/dashboard.ts
+++ swimapp-frontend/src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { ParentService } from '../../services/parent/parent.service';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,7 @@ import { SwimmerSummaryDto } from '../../interfaces/swimmer-summary-dto';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { SwimmerService } from '../../services/swimmer/swimmer.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,13 +17,18 @@ import { MatCardModule } from '@angular/material/card';
 export class Dashboard implements OnInit{
   private authService: AuthService = inject(AuthService);
   private parentService: ParentService = inject(ParentService);
+  private swimmerService: SwimmerService = inject(SwimmerService);
 
   user = this.authService.user;
   swimmers: SwimmerSummaryDto[] = [];
+  swimmersList = signal<SwimmerSummaryDto[]>([]);
 
   ngOnInit(): void {
     if (this.user()?.roles?.includes('PARENT')) {
       this.getMySwimmers();
+    }
+    if (this.user()?.roles?.includes('ADMIN')) {
+      this.getSwimmersList();
     }
   }
 
@@ -35,6 +41,18 @@ export class Dashboard implements OnInit{
       error: error => {
         console.log("Failed to load swimmers", error);
       }
-    })
+    });
+  }
+
+  getSwimmersList() {
+    this.swimmerService.getSwimmersList().subscribe({
+      next: (swimmersList) => {
+        console.log("SWIMMERS: ", swimmersList);
+        this.swimmersList.set(swimmersList);
+      },
+      error: error => {
+        console.log("Failed to load swimmers", error);
+      }
+    });
   }
 }
--- swimapp-frontend/src/app/services/swimmer/swimmer.service.ts
+++ swimapp-frontend/src/app/services/swimmer/swimmer.service.ts
@@ -1,16 +1,22 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { CreateSwimmerDto } from '../../interfaces/create-swimmer-dto';
+import { SwimmerSummaryDto } from '../../interfaces/swimmer-summary-dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwimmerService {
   createSwimmerUrl = 'http://localhost:8080/swimmers';
-
+  getSwimmersListUrl = 'http://localhost:8080/swimmers';
   private http: HttpClient = inject(HttpClient);
 
   createSwimmer(dto: CreateSwimmerDto) {
     return this.http.post<CreateSwimmerDto>(this.createSwimmerUrl, dto);
+  }
+
+  getSwimmersList(): Observable<SwimmerSummaryDto[]> {
+    return this.http.get<SwimmerSummaryDto[]>(this.getSwimmersListUrl);
   }
 }
