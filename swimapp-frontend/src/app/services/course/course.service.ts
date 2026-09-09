1ffba8219d9218774beb7a3a7e8c569927c41fc6
Dio Utoro <dioutoro@googlemail.com>
2026-09-09 16:59:18 +0200

    add get all courses method

--- swimapp-frontend/src/app/services/course/course.service.ts
+++ swimapp-frontend/src/app/services/course/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
 import { CreateCourseDto } from '../../interfaces/create-course-dto';
+import { CourseSummaryDto } from '../../interfaces/course-summary-dto';
 
 
 @Injectable({
@@ -8,10 +9,15 @@ import { CreateCourseDto } from '../../interfaces/create-course-dto';
 })
 export class CourseService {
   createCourseUrl = 'http://localhost:8080/courses';
-
+  getCoursesListUrl = 'http://localhost:8080/courses';
   private http: HttpClient = inject(HttpClient);
 
   createCourse(dto: CreateCourseDto) {
     return this.http.post<CreateCourseDto>(this.createCourseUrl, dto);
   }
+
+  getAllCourses() {
+    return this.http.get<CourseSummaryDto[]>(this.getCoursesListUrl);
+  }
+
 }
