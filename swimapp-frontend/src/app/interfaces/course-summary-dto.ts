617062f828ea60a97233a2d86e9a85f7fec0def0
Dio Utoro <dioutoro@googlemail.com>
2026-09-09 16:57:57 +0200

    add course summary dto for admin dashboard

---
+++ swimapp-frontend/src/app/interfaces/course-summary-dto.ts
@@ -0,0 +1,9 @@
+export interface CourseSummaryDto {
+  courseId: number,
+  courseName: string,
+  courseDay: string,
+  courseTime: string,
+  trainerFirstName: string,
+  trainerLastName: string,
+  levelName: string,
+}
