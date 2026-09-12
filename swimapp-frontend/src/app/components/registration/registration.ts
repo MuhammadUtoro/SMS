b08270c965bafc77a16021e18577c91da2fbbd3d
Dio Utoro <dioutoro@googlemail.com>
2026-09-12 09:19:28 +0200

    add logic for editing profile page

--- swimapp-frontend/src/app/components/registration/registration.ts
+++ swimapp-frontend/src/app/components/registration/registration.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -7,7 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ParentService } from '../../services/parent/parent.service';
 import { KeycloakService } from '../../services/keycloak/keycloak.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -23,10 +23,13 @@ import { Router } from '@angular/router';
   styleUrl: './registration.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Registration {
-  private parentService = inject(ParentService);
-  private keycloakService = inject(KeycloakService);
-  private router = inject(Router);
+export class Registration implements OnInit{
+  private parentService: ParentService = inject(ParentService);
+  private keycloakService: KeycloakService = inject(KeycloakService);
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  isEditMode = signal(this.router.url === '/parents/me/edit');
 
   form: FormGroup = new FormGroup({
     email: new FormControl(''),
@@ -35,6 +38,12 @@ export class Registration {
     username: new FormControl(''),
     password: new FormControl(''),
   });
+
+  ngOnInit() {
+    if (this.router.url === '/parents/me/edit') {
+      this.loadParentProfile();
+    }
+  }
 
   // Method to register parent
   registerParent() {
@@ -52,5 +61,21 @@ export class Registration {
   // To login user after registration
   login() {
     this.keycloakService.login();
+  }
+
+  loadParentProfile() {
+    this.parentService.getMyProfile().subscribe({
+      next: (parent) => {
+        this.form.patchValue({
+          email: parent.email,
+          firstName: parent.firstName,
+          lastName: parent.lastName,
+          username: parent.username
+        });
+      },
+      error: (error) => {
+        console.log("Failed to load profile!", error);
+      }
+    });
   }
 }
