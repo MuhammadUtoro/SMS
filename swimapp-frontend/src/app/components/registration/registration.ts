7ab846fc03d05c6c10f729abbd78108271414126
Dio Utoro <dioutoro@googlemail.com>
2026-09-14 22:41:43 +0200

    adjust submit button to call updateMyProfile()

--- swimapp-frontend/src/app/components/registration/registration.ts
+++ swimapp-frontend/src/app/components/registration/registration.ts
@@ -7,7 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ParentService } from '../../services/parent/parent.service';
 import { KeycloakService } from '../../services/keycloak/keycloak.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -27,7 +27,6 @@ export class Registration implements OnInit{
   private parentService: ParentService = inject(ParentService);
   private keycloakService: KeycloakService = inject(KeycloakService);
   private router: Router = inject(Router);
-  private route: ActivatedRoute = inject(ActivatedRoute);
 
   isEditMode = signal(this.router.url === '/parents/me/edit');
 
@@ -63,6 +62,20 @@ export class Registration implements OnInit{
     this.keycloakService.login();
   }
 
+  // Update the email
+  updateMyProfile() {
+    this.parentService.updateMyProfile(this.form.value).subscribe({
+      next: (response) => {
+        console.log('Success!', response);
+        this.router.navigate(['parents/me']);
+      },
+      error: (error) => {
+        console.log('Failed to update!', error);
+      }
+    });
+  }
+
+  // Load the profile for checking before update
   loadParentProfile() {
     this.parentService.getMyProfile().subscribe({
       next: (parent) => {
