4a5ea1e74fcda9ff57e2ef4a39cdc0d61307e5e5
Dio Utoro <dioutoro@googlemail.com>
2026-09-17 00:46:58 +0200

    add url for update info, course and level

--- swimapp-frontend/src/app/services/swimmer/swimmer.service.ts
+++ swimapp-frontend/src/app/services/swimmer/swimmer.service.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CreateSwimmerDto } from '../../interfaces/create-swimmer-dto';
 import { SwimmerSummaryDto } from '../../interfaces/swimmer-summary-dto';
+import { UpdateSwimmerInfoDto } from '../../interfaces/update-swimmer-info-dto';
+import { UpdateSwimmerCourseDto } from '../../interfaces/update-swimmer-course-dto';
+import { UpdateSwimmerLevelDto } from '../../interfaces/update-swimmer-level-dto';
+import { SwimmerDetailDto } from '../../interfaces/swimmer-detail-dto';
+
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +15,9 @@ import { SwimmerSummaryDto } from '../../interfaces/swimmer-summary-dto';
 export class SwimmerService {
   createSwimmerUrl = 'http://localhost:8080/swimmers';
   getSwimmersListUrl = 'http://localhost:8080/swimmers';
+  getSwimmerByIdUrl = 'http://localhost:8080/swimmers';
+  updateSwimmerInfoUrl = 'http://localhost:8080/swimmers';
+  updateSwimmerCourseUrl = 'http://localhost:8080/swimmers';
   private http: HttpClient = inject(HttpClient);
 
   createSwimmer(dto: CreateSwimmerDto) {
@@ -18,5 +26,29 @@ export class SwimmerService {
 
   getSwimmersList(): Observable<SwimmerSummaryDto[]> {
     return this.http.get<SwimmerSummaryDto[]>(this.getSwimmersListUrl);
+  }
+
+  getSwimmerById(swimmerId: number): Observable<SwimmerDetailDto> {
+    return this.http.get<SwimmerDetailDto>(
+      `${this.getSwimmerByIdUrl}/${swimmerId}`,
+    );
+  }
+
+  updateSwimmerInfo(swimmerId: number, dto: UpdateSwimmerInfoDto) {
+    return this.http.put<SwimmerDetailDto>(
+      `${this.updateSwimmerInfoUrl}/${swimmerId}`, dto
+    );
+  }
+
+  updateSwimmerCourse(swimmerId: number, dto: UpdateSwimmerCourseDto) {
+    return this.http.patch<SwimmerDetailDto>(
+      `${this.updateSwimmerCourseUrl}/${swimmerId}/course`, dto
+    );
+  }
+
+  updateSwimmerLevel(swimmerId: number, dto: UpdateSwimmerLevelDto) {
+    return this.http.patch<SwimmerDetailDto>(
+      `${this.updateSwimmerCourseUrl}/${swimmerId}/level`, dto
+    );
   }
 }
