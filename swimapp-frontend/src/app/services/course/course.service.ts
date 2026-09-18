daedfdd0754affd2ca31a7c8c8429e5647833ba4
Dio Utoro <dioutoro@googlemail.com>
2026-09-18 22:48:59 +0200

    add edit course info method to update course info

--- swimapp-frontend/src/app/services/course/course.service.ts
+++ swimapp-frontend/src/app/services/course/course.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { CreateCourseDto } from '../../interfaces/create-course-dto';
 import { CourseSummaryDto } from '../../interfaces/course-summary-dto';
 import { UpdateCourseInfoDto } from '../../interfaces/update-course-info-dto';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -11,8 +12,10 @@ import { UpdateCourseInfoDto } from '../../interfaces/update-course-info-dto';
 export class CourseService {
   createCourseUrl = 'http://localhost:8080/courses';
   getCoursesListUrl = 'http://localhost:8080/courses';
+  getCourseByIdUrl = 'http://localhost:8080/courses';
   updateCourseLevelUrl = 'http://localhost:8080/courses';
   updateCourseInfoUrl = 'http://localhost:8080/courses';
+  deleteCourseUrl = 'http://localhost:8080/courses';
   private http: HttpClient = inject(HttpClient);
 
   createCourse(dto: CreateCourseDto) {
@@ -23,8 +26,19 @@ export class CourseService {
     return this.http.get<CourseSummaryDto[]>(this.getCoursesListUrl);
   }
 
+  getCourseById(courseId: number): Observable<CourseSummaryDto> {
+    return this.http.get<CourseSummaryDto>(
+      `${this.getCourseByIdUrl}/${courseId}`,
+    );
+  }
+
   updateCourseInfo(dto: UpdateCourseInfoDto) {
     return this.http.put<CourseSummaryDto>(this.updateCourseInfoUrl, dto);
   }
 
+  deleteCourse(courseId: number) {
+    return this.http.delete(
+      `${this.deleteCourseUrl}/${courseId}`
+    );
+  }
 }
