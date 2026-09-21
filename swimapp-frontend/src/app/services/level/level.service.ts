3c0e5a64dd910d00bf2af00e28b5233f3f2c48c9
Dio Utoro <dioutoro@googlemail.com>
2026-09-22 00:26:31 +0200

    add method to get level by Id

--- swimapp-frontend/src/app/services/level/level.service.ts
+++ swimapp-frontend/src/app/services/level/level.service.ts
@@ -2,12 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LevelSummaryDto } from '../../interfaces/level-summary-dto';
 import { CreateLevelDto } from '../../interfaces/create-level-dto';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LevelService {
   getLevelsList = 'http://localhost:8080/levels';
+  getLevelByIdUrl = 'http://localhost:8080/levels';
   createLevelUrl = 'http://localhost:8080/levels'
   private http: HttpClient = inject(HttpClient);
 
@@ -17,5 +19,11 @@ export class LevelService {
 
   createLevel(dto: CreateLevelDto) {
     return this.http.post<CreateLevelDto>(this.createLevelUrl, dto);
+  }
+
+  getLevelById(levelId: number): Observable<LevelSummaryDto> {
+    return this.http.get<LevelSummaryDto>(
+      `${this.getLevelByIdUrl}/${levelId}`,
+    );
   }
 }
