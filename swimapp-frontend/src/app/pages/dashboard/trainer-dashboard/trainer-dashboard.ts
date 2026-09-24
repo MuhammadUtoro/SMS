2bbcd73571f1102394419f8394296afc50165a57
Dio Utoro <dioutoro@googlemail.com>
2026-09-25 00:56:51 +0200

    move trainer dashboard from component dashboard to pages dashboard and style the dashboard

--- swimapp-frontend/src/app/pages/dashboard/trainer-dashboard/trainer-dashboard.ts
+++ swimapp-frontend/src/app/pages/dashboard/trainer-dashboard/trainer-dashboard.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { TrainerService } from '../../../services/trainer/trainer.service';
+import { TrainerSummaryDto } from '../../../interfaces/trainer-summary-dto';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-trainer-dashboard',
-  imports: [],
+  imports: [MatDividerModule],
   templateUrl: './trainer-dashboard.html',
   styleUrl: './trainer-dashboard.css',
 })
-export class TrainerDashboard {
+export class TrainerDashboard implements OnInit{
+  private trainerService: TrainerService = inject(TrainerService);
+  trainer = signal<TrainerSummaryDto | null>(null);
 
+  ngOnInit(): void {
+    this.trainerService.getMyProfile().subscribe({
+      next: (trainer) => {
+        this.trainer.set(trainer);
+      },
+      error: (error) => {
+        console.log("Failed to load profile!", error);
+      }
+    });
+  }
 }
