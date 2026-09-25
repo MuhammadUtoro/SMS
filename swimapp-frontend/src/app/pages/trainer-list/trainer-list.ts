5fba7eb15150092a9512babc4bfb43a4efb1f87b
Dio Utoro <dioutoro@googlemail.com>
2026-09-25 23:43:43 +0200

    add trainer list page

---
+++ swimapp-frontend/src/app/pages/trainer-list/trainer-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-list',
+  imports: [],
+  templateUrl: './trainer-list.html',
+  styleUrl: './trainer-list.css',
+})
+export class TrainerList {
+
+}
